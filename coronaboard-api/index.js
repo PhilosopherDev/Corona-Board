911d276d0c2f9de92d9ace324300026937eb3a99
PhilosopherProgrammer <nekarakube@gmail.com>
2022-07-03 16:36:27 +0900

    connect key-value route

--- coronaboard-api/index.js
+++ coronaboard-api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { sequelize } = require('./database');
 const globalStatController = require('./controller/global-stat.controller');
+const keyValueController = require('./controller/key-value.controller');
 
 async function launchServer() {
     const app = express();
@@ -16,6 +17,11 @@ async function launchServer() {
     app.get('/global-stats', globalStatController.getAll);
     app.post('/global-stats', globalStatController.insertOrUpdate);
     app.delete('/global-stats', globalStatController.remove);
+
+    // Key-Value
+    app.get('/key-value/:key', keyValueController.get);
+    app.post('/key-value', keyValueController.insertOrUpdate);
+    app.delete('/key-value/:key', keyValueController.remove);
 
     try {
         await sequelize.sync();
